96311c2cd78ee99f8e8c89ead1dbf0625b154443
dilyanrusev <dilyanrusev@b383517c-aeda-11de-b4c2-c9e47ae42fda>
2009-10-02 00:26:19 +0000

    added a font and some basic graphics

--- foo-hang.py
+++ foo-hang.py
@@ -1,4 +1,8 @@
 import random
+import pygame
+from pygame.locals import *
+
+font = None
 
 def loadWords():
     "Load a list of words that the user should try to guess"
@@ -21,7 +25,44 @@ def getRandomWord(words):
     randomWord = words.keys()[indexOfRandomWord]
     return randomWord
 
+def showWindow():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Foo-Hang")
+    return screen
+
+def loadFont():
+    global font
+    if font is None:
+        font = pygame.font.Font("AgentOrange.ttf", 32)
+
+def drawText(screen, x, y, text):
+    global font
+    loadFont()
+    text = font.render(text, True, (255, 255, 255), (0,0,0))
+    text = text.convert()
+    text.set_colorkey((0,0,0))
+    screen.blit(text, (x,y))
+
+def runLoop(screen):
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                done = True
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                done = True
+        if not done:
+            drawText(screen, 10, 10, "Hello, world!")
+            pygame.display.flip()
+
 
 if __name__ == "__main__":
-    words = loadWords()
-    print getRandomWord(words)
+    #words = loadWords()
+    #print getRandomWord(words)
+    screen = None
+    try:
+        screen = showWindow()
+        runLoop(screen)
+    finally:
+        pygame.quit()
